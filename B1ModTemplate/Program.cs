cefa0a3113eef8c5887ca1c3099e5e33ff823fa4
lxymahatma <lxymahatma@gmail.com>
2024-10-19 19:09:31 +0900

    initial commit for mod template

---
+++ B1ModTemplate/Program.cs
@@ -0,0 +1,46 @@
+﻿using CSharpModBase.Input;
+
+// using HarmonyLib;
+
+namespace CSharpExample;
+
+public sealed class MyMod : ICSharpMod
+{
+    // private readonly Harmony harmony;
+
+    public string Name => ModName;
+    public string Version => ModVersion;
+
+    public void Init()
+    {
+        Console.WriteLine($"{Name} Init");
+        Utils.RegisterKeyBind(Key.ENTER, () => Console.WriteLine("Enter pressed"));
+        Utils.RegisterKeyBind(ModifierKeys.Control, Key.ENTER, FindPlayer);
+
+        // hook
+        // harmony.PatchAll();
+    }
+
+    public void DeInit()
+    {
+        Console.WriteLine($"{Name} DeInit");
+        // harmony.UnpatchAll();
+    }
+
+    private static void FindPlayer()
+    {
+        Console.WriteLine("Ctrl+Enter pressed");
+        var player = MyUtils.GetControlledPawn();
+        if (player == null)
+        {
+            Console.WriteLine("Player not found");
+        }
+        else
+        {
+            Console.WriteLine($"Player found: {player}");
+            var hp = BGUFunctionLibraryCS.GetAttrValue(player, EBGUAttrFloat.Hp);
+            var hpMax = BGUFunctionLibraryCS.GetAttrValue(player, EBGUAttrFloat.HpMax);
+            Console.WriteLine($"HP: {hp}/{hpMax}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# B1.Mod.Template
+
+Template for making Black Myth Wukong C# Mods
